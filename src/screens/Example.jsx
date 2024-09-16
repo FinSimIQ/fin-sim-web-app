49542be208c290e63875c7119cc3750bd979e7cd
Laksh Makhija <76146478+lakimapturn@users.noreply.github.com>
2024-09-15 23:46:23 -0400

    removed unnecessary dependencies

--- src/screens/Example.jsx
+++ src/screens/Example.jsx
@@ -1,10 +1,4 @@
-import {
-  Code,
-  Container,
-  ListItem,
-  UnorderedList,
-  VStack,
-} from "@chakra-ui/react";
+import { Code, Container, ListItem, UnorderedList } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const Example = (props) => {
