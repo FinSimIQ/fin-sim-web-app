90ac5aedacd9d15cf5f282e591bc2b2f2f42ac77
nyccreator <andyfernandeznyc@gmail.com>
2024-09-24 06:34:10 -0400

    feat: create landing page content

--- src/screens/Landing.jsx
+++ src/screens/Landing.jsx
@@ -15,6 +15,12 @@ import {
 } from "@chakra-ui/react";
 import LogoImage from "../assets/logo.svg";
 import HeaderImage from "../assets/header.svg";
+import LeftLandingCard from "../assets/leftLandingCard.svg";
+import MiddleLandingCard from "../assets/middleLandingCard.svg";
+import RightLandingCard from "../assets/rightLandingCard.svg";
+import LeftLandingCardImage from "../assets/leftLandingCardImage.svg";
+import MiddleLandingCardImage from "../assets/middleLandingCardImage.svg";
+import RightLandingCardImage from "../assets/rightLandingCardImage.svg";
 import { ArrowForwardIcon } from "@chakra-ui/icons";
 
 const Landing = () => {
@@ -33,8 +39,10 @@ const Landing = () => {
 				fontWeight="semibold"
 				letterSpacing="tightish"
 				fontSize="lg"
+				boxShadow="md"
+				zIndex="1"
 			>
-				<Image src={LogoImage} alt="Logo" h="12" />
+				<Image src={LogoImage} alt="Logo" h="12" draggable="false" />
 				<Spacer />
 				<Text my="4">Home</Text>
 				<Spacer />
@@ -50,8 +58,7 @@ const Landing = () => {
 			</Flex>
 			<Container minW="100%">
 				<HStack px="4" mt="20" mb="32" justify="center">
-					<Image src={HeaderImage} alt="Header" w="50%" />
-					{/* <Spacer /> */}
+					<Image src={HeaderImage} alt="Header" w="50%" draggable="false" />
 					<VStack textAlign="center">
 						<HStack spacing="0" fontWeight="bold" letterSpacing="tightest">
 							<Text fontSize="6xl" color="white">
@@ -85,8 +92,109 @@ const Landing = () => {
 					</VStack>
 				</HStack>
 			</Container>
-			<Container minW="100%" bg="#F4F4F4">
-				<Text>Hello</Text>
+			<Container minW="100%" py="6" px="16" bg="#F4F4F4">
+				<HStack pb="16" align="baseline">
+					<Container maxW="lg" fontSize="4xl" fontWeight="semibold">
+						Learn Finance and Stock Market Concepts
+					</Container>
+					<Spacer />
+					<Text maxW="3xl" fontFamily="metrophobic" fontSize="xl">
+						Explore real-world stock simulations, tackle interactive finance
+						quizzes, and navigate dynamic market challenges. Enhance your
+						strategy development, refine your decision-making skills, and build
+						your financial confidence.
+					</Text>
+				</HStack>
+				<HStack justify="space-between">
+					<Container
+						minH="xs"
+						maxH="xl"
+						maxW="xs"
+						px="8"
+						py="4"
+						m="0"
+						bgImage={LeftLandingCard}
+						bgRepeat="no-repeat"
+						bgSize="contain"
+						bgPos="center"
+					>
+						<Text maxW="sm" fontWeight="medium" fontSize="2xl">
+							Interactive Finance Quizzes
+						</Text>
+						<Text maxW="sm" py="2" fontFamily="metrophobic" fontSize="md">
+							Learn finance and stock market concepts through complex and
+							interactive quizzes
+						</Text>
+						<HStack>
+							<Image
+								src={LeftLandingCardImage}
+								alt="Header"
+								w="40%"
+								draggable="false"
+							/>
+							<Spacer />
+						</HStack>
+					</Container>
+					<Container
+						minH="xs"
+						maxH="xl"
+						maxW="xs"
+						px="8"
+						py="8"
+						m="0"
+						bgImage={MiddleLandingCard}
+						bgRepeat="no-repeat"
+						bgSize="contain"
+						bgPos="center"
+					>
+						<HStack>
+							<Image
+								src={MiddleLandingCardImage}
+								alt="Header"
+								w="40%"
+								draggable="false"
+							/>
+							<Spacer />
+						</HStack>
+						<Text maxW="md" pt="4" pb="2" fontWeight="medium" fontSize="2xl">
+							Learn with Friends
+						</Text>
+						<Text maxW="sm" fontFamily="metrophobic" fontSize="md">
+							Discover friends to learn with and find encouragement along the
+							way by earning badges with completed lessons
+						</Text>
+					</Container>
+					<Container
+						minH="xs"
+						maxH="xl"
+						maxW="xs"
+						px="8"
+						py="4"
+						m="0"
+						textAlign="right"
+						bgImage={RightLandingCard}
+						bgRepeat="no-repeat"
+						bgSize="contain"
+						bgPos="center"
+					>
+						<Text maxW="sm" fontWeight="medium" fontSize="2xl">
+							Weekly Market Challenges
+						</Text>
+						<Text maxW="sm" py="2" fontFamily="metrophobic" fontSize="md">
+							Enhance your strategy development and critical thinking skills by
+							engaging in virtual trading scenarios
+						</Text>
+						<HStack>
+							<Spacer />
+							<Image
+								src={RightLandingCardImage}
+								alt="Header"
+								w="40%"
+								draggable="false"
+							/>
+						</HStack>
+					</Container>
+				</HStack>
 			</Container>
 		</Container>
 	);
