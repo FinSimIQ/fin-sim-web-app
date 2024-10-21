ec2b80815e30dd2cd4fb84f2c70f270517404e0c
yydeng1 <ydeng1025@gmail.com>
2024-10-20 23:24:35 -0400

    added svg file and background for user authentication pages

--- src/screens/Login.jsx
+++ src/screens/Login.jsx
@@ -1,35 +1,55 @@
-import React from 'react';
-import { Flex, Box, Input, InputGroup, InputLeftElement, Button, Text, Link } from '@chakra-ui/react';
-import { EmailIcon, LockIcon } from '@chakra-ui/icons';
+import React, { useState } from 'react';
+import { Flex, Box, Input, InputGroup, InputLeftElement, InputRightElement, Button, Text, Link, FormControl, FormLabel } from '@chakra-ui/react';
+import { EmailIcon, LockIcon, ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import wave from "../assets/wave.svg";
 
 const Login = () => {
+  // password visibility
+  const [showPassword, setShowPassword] = useState(false);
+  const handlePasswordToggle = () => setShowPassword(!showPassword);
+
   return (
     <Flex h="100vh">
       <Box flex="1" bg="white" p="8" display="flex" alignItems="center" justifyContent="center">
         <Box w="100%" maxW="400px">
-          <Text fontSize="5xl" fontWeight="bold" mb="4">
+
+          <Text fontSize="4xl" fontWeight="bold" textAlign="center" mb="4" color="gray.600">
             Sign In
           </Text>
-          <Text mb="8" color="gray.500">
+          <Text textAlign="center" mb="8" color="gray.600">
             Welcome back! Please enter your details.
           </Text>
           
-          <InputGroup mb="4">
-            <InputLeftElement pointerEvents="none">
-              <EmailIcon color="gray.400" />
-            </InputLeftElement>
-            <Input type="email" placeholder="Enter Your Email" />
-          </InputGroup>
+          <FormControl mb="4">
+            <FormLabel fontWeight="bold">Email</FormLabel>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <EmailIcon color="gray.400" />
+              </InputLeftElement>
+              <Input type="email" placeholder="Enter Your Email" />
+            </InputGroup>
+          </FormControl>
 
-          <InputGroup mb="4">
-            <InputLeftElement pointerEvents="none">
-              <LockIcon color="gray.400" />
-            </InputLeftElement>
-            <Input type="password" placeholder="Enter Your Password" />
-          </InputGroup>
+          <FormControl mb="4">
+            <FormLabel fontWeight="bold">Password</FormLabel>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <LockIcon color="gray.400" />
+              </InputLeftElement>
+              <Input
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Enter Your Password"
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handlePasswordToggle} variant="ghost">
+                  {showPassword ? <ViewOffIcon color="gray.400" /> : <ViewIcon color="gray.400" />}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
 
           <Box textAlign="right" mb="6">
-            <Link color="gray.500" href="/forgotpassword">
+            <Link color="gray.600" fontWeight="bold" href="/forgotpassword">
               Forgot Password?
             </Link>
           </Box>
@@ -38,18 +58,43 @@ const Login = () => {
             Sign In
           </Button>
 
-          <Text textAlign="center">
+          <Text textAlign="center" color="gray.500">
             Don't have an account?{' '}
-            <Link color="green.500" href="/signup">
+            <Link color="green.500" href="/signup" fontWeight="bold">
               Sign up
             </Link>
           </Text>
         </Box>
       </Box>
 
-      <Box flex="1" bg="green.100" display={{ base: 'none', md: 'block' }}>
-        <Box h="100%" bg="green.200" backgroundSize="cover" />
+      {/* background */}
+      <Box position="relative" flex="1" display={{ base: 'none', md: 'block' }}>
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+          bgGradient="linear(to-t, green.900, green.100)"
+          zIndex="1"
+        />
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+          bgImage={`url(${wave})`}
+          bgSize="cover"
+          bgRepeat="no-repeat"
+          bgPosition="center"
+          zIndex="2"
+          bgColor="green.500"
+          backgroundBlendMode="overlay"
+          opacity="0.75"
+        />
       </Box>
+
     </Flex>
   );
 };
--- src/screens/Signup.jsx
+++ src/screens/Signup.jsx
@@ -1,53 +1,95 @@
-import React from 'react';
-import { Flex, Box, Input, InputGroup, InputLeftElement, Button, Text, Link } from '@chakra-ui/react';
-import { EmailIcon, LockIcon } from '@chakra-ui/icons';
-import { FaUserAlt } from 'react-icons/fa';
+import React, { useState } from 'react';
+import { Flex, Box, Input, InputGroup, InputLeftElement, InputRightElement, Button, Text, Link, FormControl, FormLabel } from '@chakra-ui/react';
+import { EmailIcon, LockIcon, ViewIcon, ViewOffIcon } from '@chakra-ui/icons';
+import wave from "../assets/wave.svg";
 
 const Signup = () => {
+  // password visibility
+  const [showPassword, setShowPassword] = useState(false);
+  const handlePasswordToggle = () => setShowPassword(!showPassword);
+
   return (
     <Flex h="100vh">
       <Box flex="1" bg="white" p="8" display="flex" alignItems="center" justifyContent="center">
         <Box w="100%" maxW="400px">
-          <Text fontSize="3xl" fontWeight="bold" mb="4">
+
+          <Text fontSize="4xl" fontWeight="bold" textAlign="center" mb="4" color="gray.600">
             Create Your Account
           </Text>
           
-          <InputGroup mb="4">
-            <InputLeftElement pointerEvents="none">
-              <FaUserAlt color="gray.400" />
-            </InputLeftElement>
-            <Input type="text" placeholder="Full Name" isRequired />
-          </InputGroup>
+          <FormControl mb="4" isRequired>
+            <FormLabel fontWeight="bold">Full Name</FormLabel>
+            <Input type="text" placeholder="Full Name" />
+          </FormControl>
 
-          <InputGroup mb="4">
-            <InputLeftElement pointerEvents="none">
-              <EmailIcon color="gray.400" />
-            </InputLeftElement>
-            <Input type="email" placeholder="Enter Your Email" isRequired />
-          </InputGroup>
+          <FormControl mb="4" isRequired>
+            <FormLabel fontWeight="bold">Email</FormLabel>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <EmailIcon color="gray.400" />
+              </InputLeftElement>
+              <Input type="email" placeholder="Enter Your Email" />
+            </InputGroup>
+          </FormControl>
 
-          <InputGroup mb="4">
-            <InputLeftElement pointerEvents="none">
-              <LockIcon color="gray.400" />
-            </InputLeftElement>
-            <Input type="password" placeholder="Enter Your Password" isRequired />
-          </InputGroup>
+          <FormControl mb="4" isRequired>
+            <FormLabel fontWeight="bold">Password</FormLabel>
+            <InputGroup>
+              <InputLeftElement pointerEvents="none">
+                <LockIcon color="gray.400" />
+              </InputLeftElement>
+              <Input
+                type={showPassword ? 'text' : 'password'}
+                placeholder="Enter Your Password"
+              />
+              <InputRightElement width="4.5rem">
+                <Button h="1.75rem" size="sm" onClick={handlePasswordToggle} variant="ghost">
+                  {showPassword ? <ViewOffIcon color="gray.400" /> : <ViewIcon color="gray.400" />}
+                </Button>
+              </InputRightElement>
+            </InputGroup>
+          </FormControl>
 
-          <Button colorScheme="green" size="lg" w="100%" mb="4">
+
+          <Button colorScheme="green" size="lg" w="100%" mb="4" marginTop="5">
             Sign Up
           </Button>
 
-          <Text textAlign="center">
+          <Text textAlign="center" color="gray.500">
             Already have an account?{' '}
-            <Link color="green.500" href="#">
+            <Link color="green.500" href="/login" fontWeight="bold">
               Sign in
             </Link>
           </Text>
         </Box>
       </Box>
 
-      <Box flex="1" bg="green.100" display={{ base: 'none', md: 'block' }}>
-        <Box h="100%" bg="green.200" backgroundSize="cover" />
+      {/* background */}
+      <Box position="relative" flex="1" display={{ base: 'none', md: 'block' }}>
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+          bgGradient="linear(to-t, green.900, green.100)"
+          zIndex="1"
+        />
+        <Box
+          position="absolute"
+          top="0"
+          left="0"
+          right="0"
+          bottom="0"
+          bgImage={`url(${wave})`}
+          bgSize="cover"
+          bgRepeat="no-repeat"
+          bgPosition="center"
+          zIndex="2"
+          bgColor="green.500"
+          backgroundBlendMode="overlay"
+          opacity="0.75"
+        />
       </Box>
     </Flex>
   );
