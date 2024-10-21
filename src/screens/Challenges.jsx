a1b05297e46c8d840b71ee5725e18dd1167decbb
erika-tay <147895704+erika-tay@users.noreply.github.com>
2024-10-21 13:32:10 -0400

    added question 2

--- src/screens/Challenges.jsx
+++ src/screens/Challenges.jsx
@@ -10,7 +10,7 @@ import React, { useState } from "react";
 import Navbar from "../components/NavBar";
 
 const Challenges = (props) => {
-  // for question 1
+  // for QUESTION 1
   const [selectedAnswer, setSelectedAnswer] = useState(null);
   const [isSubmitted, setIsSubmitted] = useState(false);
   const [isCorrect, setIsCorrect] = useState(false);
@@ -69,11 +69,70 @@ const Challenges = (props) => {
     submitButtonText = "Correct!";
   }
 
+  // for QUESTION 2
+  const [selectedAnswer2, setSelectedAnswer2] = useState(null);
+  const [isSubmitted2, setIsSubmitted2] = useState(false);
+  const [isCorrect2, setIsCorrect2] = useState(false);
+
+  // assign the correct answer for question 1 to check later
+  const correctAnswer2 = "C";
+
+  // handle answer selection
+  const handleAnswerClick2 = (answer) => {
+    if (!isSubmitted2) {
+      setSelectedAnswer2(answer);
+    }
+  };
+
+  // handle submission
+  const handleSubmit2 = () => {
+    if (!isSubmitted2) {
+      setIsSubmitted2(true);
+      setIsCorrect2(selectedAnswer2 === correctAnswer2);
+    } else {
+      setIsSubmitted2(false);
+      setSelectedAnswer2(null);
+      setIsCorrect2(false);
+    }
+  };
+
+  // getting and setting answer button bg color
+  // hover and pressed
+  const getButtonColor2 = (answer) => {
+    if (isSubmitted2) {
+      if (answer === selectedAnswer2) {
+        return answer === correctAnswer2 ? "#42D674" : "#FFADA8"; // Green if correct, red if wrong
+      } else {
+        return "#FFFFFF";
+      }
+    }
+    if (!isSubmitted2 & (selectedAnswer2 != null)) {
+      if (answer === selectedAnswer2) {
+        return "#E3E3E3";
+      } else {
+        return "#FFFFFF";
+      }
+    }
+    if (!isSubmitted2 & (selectedAnswer2 == null)) {
+      return "#FFFFFF";
+    }
+  };
+
+  var submitButtonText2;
+
+  if (!isSubmitted2) {
+    submitButtonText2 = "Submit Answer";
+  } else if (isSubmitted2 && !isCorrect2) {
+    submitButtonText2 = "Try Again!";
+  } else if (isSubmitted2 && isCorrect2) {
+    submitButtonText2 = "Correct!";
+  }
+
   return (
     <Container minW="100%" p="0" m="0" bg="#D9D9D9">
       <Navbar></Navbar>
       <Container maxW="100%" py="65" background="#F1F1F1">
-        <VStack spacing="4" align="stretch">
+        <VStack spacing="10" align="stretch">
           <Container
             minW="100%"
             bg="#316D60"
@@ -201,7 +260,7 @@ const Challenges = (props) => {
                 </Text>
               </Button>
             </Flex>
-            {/* Right-aligned Submit Button */}
+            {/* Submit Button */}
             <Flex justifyContent="flex-end" mt="4">
               <Button
                 bg="#316D60"
@@ -216,7 +275,138 @@ const Challenges = (props) => {
               </Button>
             </Flex>
           </Container>
-          {/*// Question Container 2 // Question Container 3*/}
+          {/* Question Container 2 */}
+          <Container
+            minW="100%"
+            bg="#FFFFFF"
+            color="#3B3B3B"
+            borderRadius="15"
+            padding="4"
+          >
+            <Heading fontSize="28px">Economic Recession</Heading>
+            <Text fontSize="18px" py="1">
+              Scenario:
+            </Text>
+            <Text fontSize="12px">
+              The economy has entered a recession, leading to declining consumer
+              spending and rising unemployment. Many companies are issuing
+              profit warnings, and overall market sentiment is pessimistic. You
+              currently hold a variety of stocks, but the majority are in
+              consumer discretionary sectors, which are highly sensitive to
+              economic cycles. You’re trying to decide the best course of action
+              to manage your investments during this recession.
+            </Text>
+            <Text fontSize="18px" py="1">
+              Question:
+            </Text>
+            <Text fontSize="12px">
+              What is the most prudent investment strategy to protect your
+              portfolio during an economic downturn?
+            </Text>
+            <Flex
+              flexDirection={"column"}
+              direction={"LTR"}
+              flexWrap={true}
+              gap={3}
+              py="4"
+            >
+              <Button
+                bg={getButtonColor2("A")}
+                borderColor="#E3E3E3"
+                borderWidth="2px"
+                _hover={{ bg: "#E3E3E3" }}
+                _active={{
+                  bg: "#E3E3E3",
+                  transform: "scale(0.98)",
+                }}
+                display="inline-block"
+                overflow="hidden"
+                whiteSpace="normal"
+                onClick={() => handleAnswerClick2("A")}
+              >
+                <Text fontSize="12px" textAlign="left">
+                  Move a large portion of your investments into cash to avoid
+                  further losses.
+                </Text>
+              </Button>
+              <Button
+                bg={getButtonColor2("B")}
+                borderColor="#E3E3E3"
+                borderWidth="2px"
+                _hover={{ bg: "#E3E3E3" }}
+                _active={{
+                  bg: "#E3E3E3",
+                  transform: "scale(0.98)",
+                  borderColor: "#51B276",
+                }}
+                display="inline-block"
+                overflow="hidden"
+                whiteSpace="normal"
+                onClick={() => handleAnswerClick2("B")}
+              >
+                <Text fontSize="12px" textAlign="left">
+                  Continue holding your consumer discretionary stocks, as the
+                  market will eventually recover.
+                </Text>
+              </Button>
+              <Button
+                bg={getButtonColor2("C")}
+                borderColor="#E3E3E3"
+                borderWidth="2px"
+                _hover={{ bg: "#E3E3E3" }}
+                _active={{
+                  bg: "#E3E3E3",
+                  transform: "scale(0.98)",
+                  borderColor: "#51B276",
+                }}
+                display="inline-block"
+                overflow="hidden"
+                whiteSpace="normal"
+                onClick={() => handleAnswerClick2("C")}
+              >
+                <Text fontSize="12px" textAlign="left">
+                  Shift some of your investments into sectors like utilities and
+                  healthcare that tend to perform well during recessions.
+                </Text>
+              </Button>
+              <Button
+                bg={getButtonColor2("D")}
+                borderColor="#E3E3E3"
+                borderWidth="2px"
+                border
+                _hover={{ bg: "#E3E3E3" }}
+                _active={{
+                  bg: "#E3E3E3",
+                  transform: "scale(0.98)",
+                  borderColor: "#51B276",
+                }}
+                display="inline-block"
+                overflow="hidden"
+                whiteSpace="normal"
+                onClick={() => handleAnswerClick2("D")}
+              >
+                <Text fontSize="12px" textAlign="left">
+                  Increase your investments in high-growth stocks, as they are
+                  likely to offer better returns when the recession ends.
+                </Text>
+              </Button>
+            </Flex>
+            {/* Submit Button */}
+            <Flex justifyContent="flex-end" mt="4">
+              <Button
+                bg="#316D60"
+                paddingLeft="10"
+                paddingRight="10"
+                color="#FFFFFF"
+                borderRadius="20"
+                fontSize="14px"
+                onClick={handleSubmit2}
+              >
+                {submitButtonText2}
+              </Button>
+            </Flex>
+          </Container>
+          {/* Question 3 etc... */}
         </VStack>
       </Container>
     </Container>
