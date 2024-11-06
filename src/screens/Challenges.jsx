3fb29a1e3191edd82a0e3d3e4ac82bf777e4c61c
Phung Nguyen <phunggnguyenn@outlook.com>
2024-11-06 15:39:17 -0500

    Fixed background color in Challenges

--- src/screens/Challenges.jsx
+++ src/screens/Challenges.jsx
@@ -128,10 +128,149 @@ const Challenges = (props) => {
     }
   }, [index]);
 
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // set a useState loading variable to false so that the ui updates from the loader
+    if (quiz !== null) {
+      setLoading(false);
+    }
+  }, [quiz]);
+
+  // question;
+  // correctAnswer;
+  // options[list];
+
+  // for QUESTION 2
+  const [selectedAnswer2, setSelectedAnswer2] = useState(null);
+  const [isSubmitted2, setIsSubmitted2] = useState(false);
+  const [isCorrect2, setIsCorrect2] = useState(false);
+
+  // assign the correct answer for question 1 to check later
+  const correctAnswer2 = "C";
+
+  // handle answer selection
+  const handleAnswerClick2 = (answer) => {
+    if (!isSubmitted2) {
+      setSelectedAnswer2(answer);
+    }
+  };
+
+  // handle submission
+  const handleSubmit2 = () => {
+    if (!isSubmitted2) {
+      setIsSubmitted2(true);
+      setIsCorrect2(selectedAnswer2 === correctAnswer2);
+    } else {
+      setIsSubmitted2(false);
+      setSelectedAnswer2(null);
+      setIsCorrect2(false);
+    }
+  };
+
+  // getting and setting answer button bg color
+  // hover and pressed
+  const getButtonColor2 = (answer) => {
+    if (isSubmitted2) {
+      if (answer === selectedAnswer2) {
+        return answer === correctAnswer2 ? "#42D674" : "#FFADA8"; // Green if correct, red if wrong
+      } else {
+        return "#FFFFFF";
+      }
+    }
+    if (!isSubmitted2 & (selectedAnswer2 != null)) {
+      if (answer === selectedAnswer2) {
+        return "#E3E3E3";
+      } else {
+        return "#FFFFFF";
+      }
+    }
+    if (!isSubmitted2 & (selectedAnswer2 == null)) {
+      return "#FFFFFF";
+    }
+  };
+
+  var submitButtonText2;
+
+  if (!isSubmitted2) {
+    submitButtonText2 = "Submit Answer";
+  } else if (isSubmitted2 && !isCorrect2) {
+    submitButtonText2 = "Try Again!";
+  } else if (isSubmitted2 && isCorrect2) {
+    submitButtonText2 = "Correct!";
+  }
+
+  if (loading) {
+    // return a loader which there should be a chakra ui component for
+    return (
+      <Container minW="100%" p="0" m="0">
+        <Navbar></Navbar>
+        <Container bg="#F1F1F1" m={0} maxW="container.2xl" height="100vh">
+          <VStack padding="10" spacing="10">
+            <Container
+              minW="container.xl"
+              bg="#316D60"
+              color="#FFFFFF"
+              borderRadius="15"
+              paddingY="8"
+              paddingX="6"
+            >
+              <Heading fontSize="32px">Weekly Market Challenge</Heading>
+              <Text fontSize="18px">
+                Test your market analysis skills with this week's challenge.
+              </Text>
+            </Container>
+            <Container
+              minW="container.xl"
+              bg="#FFFFFF"
+              color="#3B3B3B"
+              borderRadius="15"
+              padding="4"
+            >
+              <Flex justify="center" align="center">
+                {/*Loading Circle*/}
+                <CircularProgress isIndeterminate color="#316D60" size="100%" />
+              </Flex>
+            </Container>
+          </VStack>
+        </Container>
+      </Container>
+    );
+  }
+
+  if (allCorrect) {
+    return (
+      <Container minW="100%" p="0" m="0">
+        <Navbar></Navbar>
+        <Container bg="#D9D9D9" m={0} maxW="container.2xl" height="100vh">
+          <VStack padding="10" spacing="10">
+            <Container
+              minW="container.xl"
+              bg="#316D60"
+              color="#FFFFFF"
+              borderRadius="15"
+              paddingY="8"
+              paddingX="6"
+            >
+              <Heading fontSize="32px">
+                Weekly Market Challenge Completed!
+              </Heading>
+              <Text fontSize="18px">
+                Good job! Be sure to check out the Learn page for more questions
+                to test your skills and knowledge. Don't forget to come back
+                next week for new challenge questions!
+              </Text>
+            </Container>
+          </VStack>
+        </Container>
+      </Container>
+    );
+  }
+
   return (
     <Container minW="100%" p="0" m="0">
-      <Navbar />
-      <Container bg="#F1F1F1" m={0} maxW="container.2xl" height="100vh">
+      <Navbar></Navbar>
+      <Container bg="#D9D9D9" m={0} maxW="container.2xl" height="100vh">
         <VStack padding="10" spacing="10">
           <Container
             minW="container.xl"
@@ -169,13 +308,10 @@ const Challenges = (props) => {
             <Text fontSize="18px" py="1">
               Question:
             </Text>
-            <Text fontSize="12px">
-              What is the best approach to mitigate the impact of market
-              volatility on your portfolio in this scenario?
-            </Text>
+            <Text fontSize="12px">{questionStr}</Text>
             <Flex
-              flexDirection="column"
-              direction="LTR"
+              flexDirection={"column"}
+              direction={"LTR"}
               flexWrap={true}
               gap={3}
               py="4"
@@ -195,8 +331,7 @@ const Challenges = (props) => {
                 onClick={() => handleAnswerClick("A")}
               >
                 <Text fontSize="12px" textAlign="left">
-                  Buy more shares of XYZ Corp, taking advantage of the price dip
-                  in hopes of long-term growth.
+                  {answerA}
                 </Text>
               </Button>
               <Button
@@ -207,7 +342,6 @@ const Challenges = (props) => {
                 _active={{
                   bg: "#E3E3E3",
                   transform: "scale(0.98)",
-                  borderColor: "#51B276",
                 }}
                 display="inline-block"
                 overflow="hidden"
@@ -215,7 +349,7 @@ const Challenges = (props) => {
                 onClick={() => handleAnswerClick("B")}
               >
                 <Text fontSize="12px" textAlign="left">
-                  Sell all shares of XYZ Corp to avoid further potential losses.
+                  {answerB}
                 </Text>
               </Button>
               <Button
@@ -226,7 +360,6 @@ const Challenges = (props) => {
                 _active={{
                   bg: "#E3E3E3",
                   transform: "scale(0.98)",
-                  borderColor: "#51B276",
                 }}
                 display="inline-block"
                 overflow="hidden"
@@ -234,19 +367,18 @@ const Challenges = (props) => {
                 onClick={() => handleAnswerClick("C")}
               >
                 <Text fontSize="12px" textAlign="left">
-                  Diversify your portfolio by investing in a mix of
-                  low-volatility assets, such as bonds and utilities.
+                  {answerC}
                 </Text>
               </Button>
               <Button
                 bg={getButtonColor("D")}
                 borderColor="#E3E3E3"
                 borderWidth="2px"
+                border
                 _hover={{ bg: "#E3E3E3" }}
                 _active={{
                   bg: "#E3E3E3",
                   transform: "scale(0.98)",
-                  borderColor: "#51B276",
                 }}
                 display="inline-block"
                 overflow="hidden"
@@ -254,8 +386,7 @@ const Challenges = (props) => {
                 onClick={() => handleAnswerClick("D")}
               >
                 <Text fontSize="12px" textAlign="left">
-                  Hold your current position in XYZ Corp and wait for the market
-                  to stabilize before making any changes.
+                  {answerD}
                 </Text>
               </Button>
             </Flex>
@@ -274,6 +405,95 @@ const Challenges = (props) => {
               </Button>
             </Flex>
           </Container>
+          {/* Question Container 2 */}
+          {/* Question 3 etc... */}
+          {/* <VStack>
+               <Button
+                 bg="#FFFFFF"
+                 borderColor="#E3E3E3"
+                 _hover={{ bg: "#E3E3E3" }}
+                 _active={{
+                   bg: "#42D674",
+                   transform: "scale(0.98)",
+                   borderColor: "#51B276",
+                 }}
+                 w="100%"
+                 justifyContent={"start"}
+               >
+                 <Text fontSize="12px">
+                   Buy more shares of XYZ Corp, taking advantage of the price dip
+                   in hopes of long-term growth.
+                 </Text>
+               </Button>
+               <Button
+                 bg="#FFFFFF"
+                 borderColor="#E3E3E3"
+                 _hover={{ bg: "#E3E3E3" }}
+                 _active={{
+                   bg: "#42D674",
+                   transform: "scale(0.98)",
+                   borderColor: "#51B276",
+                 }}
+                 w="100%"
+                 justifyContent={"start"}
+               >
+                 <Text fontSize="12px" textAlign="right">
+                   Sell all shares of XYZ Corp to avoid further potential losses.
+                 </Text>
+               </Button>
+               <Button
+                 bg="#FFFFFF"
+                 borderColor="#E3E3E3"
+                 _hover={{ bg: "#E3E3E3" }}
+                 _active={{
+                   bg: "#42D674",
+                   transform: "scale(0.98)",
+                   borderColor: "#51B276",
+                 }}
+                 w="100%"
+                 justifyContent={"start"}
+               >
+                 <Text fontSize="12px" textAlign="right">
+                  Diversify your portfolio by investing in a mix of
+                   low-volatility assets, such as bonds and utilities.
+                 </Text>
+               </Button>
+               <Button
+                 bg="#FFFFFF"
+                 borderColor="#E3E3E3"
+                _hover={{ bg: "#E3E3E3" }}
+               _active={{
+                   bg: "#42D674",
+                   transform: "scale(0.98)",
+                   borderColor: "#51B276",
+                 }}
+                 w="100%"
+                 justifyContent={"start"}
+               >
+                 <Text fontSize="12px" textAlign="right">
+                   Hold your current position in XYZ Corp and wait for the market
+                   to stabilize before making any changes.
+                 </Text>
+               </Button>
+               <Container
+                 minW="container.xl"
+                 display="flex"
+                 paddingRight={10}
+                 justifyContent="end"
+               >
+                 <Button
+                   bg="#316D60"
+                   paddingLeft="10"
+                   paddingRight="10"
+                   color="#FFFFFF"
+                   borderRadius="20"
+                   fontSize="14px"
+                 >
+                   Submit Answer
+                 </Button>
+               </Container>
+             </VStack>
+           </Container>*/}
         </VStack>
       </Container>
     </Container>
